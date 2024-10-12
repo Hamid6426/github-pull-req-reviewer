61ee7031b4355ef12de2420f55e1a237784c37ec
Mian Hamid Ur Rehman <mianhamid6426@gmail.com>
2024-10-13 00:56:13 +0500

    styling update 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center w-screen h-screen text-white text-2xl">
+    <div className="bg-black flex flex-col justify-center items-center w-screen h-screen text-white text-2xl">
       <div className="w-full max-w-[512px] flex flex-col justify-center items-center">
       <h1 className="text-4xl font-bold pb-8">GitHub OAuth App</h1>
       <button className="py-3 px-6 font-bold bg-green-600 hover:bg-blue-600" onClick={handleGitHubAuth}>Connect GitHub</button>
