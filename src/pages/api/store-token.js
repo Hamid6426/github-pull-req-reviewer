8b89d33af26e8177c2cceb3e5f4e6ae866a2a526
Mian Hamid Ur Rehman <mianhamid6426@gmail.com>
2024-10-13 14:10:48 +0500

    Update store-token.js

--- src/pages/api/store-token.js
+++ src/pages/api/store-token.js
@@ -1,25 +1,25 @@
 // pages/api/store-token.js
-import { MongoClient } from 'mongodb';
+import { Pool } from 'pg';
 
-const uri = process.env.MONGODB_URI;
+// Use the environment variable you just set
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_DATABASE, // Updated connection string
+});
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
     const { token } = req.body;
-    const client = new MongoClient(uri);
-    
-    try {
-      await client.connect();
-      const db = client.db('oauth-db');
-      const collection = db.collection('tokens');
 
-      await collection.insertOne({ token, createdAt: new Date() });
-      
+    try {
+      // Insert the token into the database
+      const query = 'INSERT INTO tokens(token, created_at) VALUES($1, $2)';
+      const values = [token, new Date()];
+      await pool.query(query, values);
+
       res.status(200).json({ message: 'Token stored successfully' });
     } catch (error) {
+      console.error('Error storing token:', error);
       res.status(500).json({ error: 'Failed to store token' });
-    } finally {
-      await client.close();
     }
   } else {
     res.status(405).send('Method Not Allowed');
